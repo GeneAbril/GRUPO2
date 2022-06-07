f466c5c68860bc0847fc65cfe473da964680d0e5
Carlos Iturra <carlitos0@live.cl>
2022-06-07 11:14:50 -0400

    clases Usuarios y Registros

---
+++ proyectoPlantas/src/Registros.java
@@ -0,0 +1,46 @@
+public class Registros {
+
+    //declaracion de variables
+    private Integer id;
+    private Integer planta_id;
+
+    //constructores
+
+
+    public Registros() {
+    }
+
+    public Registros(Integer id, Integer planta_id) {
+        this.id = id;
+        this.planta_id = planta_id;
+    }
+
+    //getter y setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPlanta_id() {
+        return planta_id;
+    }
+
+    public void setPlanta_id(Integer planta_id) {
+        this.planta_id = planta_id;
+    }
+
+    //metodos
+
+
+    @Override
+    public String toString() {
+        return "Registros{" +
+                "id=" + id +
+                ", planta_id=" + planta_id +
+                '}';
+    }
+}
