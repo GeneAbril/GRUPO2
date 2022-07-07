2adc5e538c260d1d6778a716faaf084d743368a4
carlos iturra gonzález <carlitos0@live.cl>
2022-07-06 23:28:34 -0400

    se añaden archivos faltantes del main - servlet y app

---
+++ ecoTank/src/main/java/com/grupo2/G1generationApplication.java
@@ -0,0 +1,13 @@
+package com.grupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G1generationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G1generationApplication.class, args);
+	}
+
+}
