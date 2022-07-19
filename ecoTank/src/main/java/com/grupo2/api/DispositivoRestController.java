170e6f7e2dc775c87e69104d3ff1bee120a42cc2
gabusho92 <g.guzman.nunez@gmail.com>
2022-07-19 17:11:58 -0400

    connect api

--- ecoTank/src/main/java/com/grupo2/api/DispositivoRestController.java
+++ ecoTank/src/main/java/com/grupo2/api/DispositivoRestController.java
@@ -4,14 +4,17 @@ package com.grupo2.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.grupo2.models.Dispositivo;
 import com.grupo2.services.DispositivoService;
 
 @RestController
 @RequestMapping("/api/dispositivos")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class DispositivoRestController {
     
     // Injeccion de dependencias
