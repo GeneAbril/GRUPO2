49f6f2a9b7648bf9d12ea0b79dba3ca392b071ac
catalinaCastilloB <ccastillobelmar@gmail.com>
2022-07-25 16:18:51 -0400

    back end comentarios

---
+++ ecoTank/src/main/java/com/grupo2/services/ComentariosServices.java
@@ -0,0 +1,35 @@
+package com.grupo2.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.grupo2.models.Comentario;
+import com.grupo2.repositories.ComentarioRepository;
+
+
+@Service
+public class ComentarioServices{
+
+    @Autowired
+    ComentarioRepository comentarioRepository;
+
+    public List<Comentario> findAll() {
+
+        return comentarioRepository.findAll();
+    }
+
+    // Metodo para enviar un dispositivo
+    public Comentario findOne(Long id) {
+        return comentarioRepository.findById(id).get();
+    }
+
+	public Comentario add(Dispositivo dispositivo) {
+		// TODO Auto-generated method stub
+		return comentarioRepository.save(dispositivo);
+	}
+
+	public void delete(Long id) {
+		comentarioRepository.deleteById(id);
+	}
+
+}
